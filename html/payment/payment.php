86581c8373145710455025c9dc92978372cdf6c4
paularoth <dic15pro@student.lu.se>
2017-09-20 12:43:56 +0200

    added redirect to receipt page

--- html/payment/payment.php
+++ html/payment/payment.php
@@ -1,16 +1,20 @@
+<?php
+session_start();
+?>
+
 <html>
 <head>
 	<title> Betalning </title>
 </head> 
 <body> 
 	<section>
-	<form id=payment>
-		<h1> Betalningsinformation</h1>
+	<form id=payment action="/payment/receipt.php" method="post">
+		<h1> Betalningsinformation för <?php echo htmlspecialchars($_SESSION["username"], ENT_QUOTES, 'UTF-8')?></h1>
 		<fieldset>	
-			<label for=email>Email</label>
+			<label for=email>Email:</label><br>
 			<input id=email name=email type=email placeholder="example@domain.com" required />	<br>
-			
-			<label for=phone>Telefon</label>
+			<br>
+			<label for=phone>Telefon:</label><br>
 			<input id=phone name=phone type=tel placeholder="0123456789" />	
 			
 		</fieldset>
@@ -29,23 +33,21 @@
 					<input id=mastercard name=cardtype type=radio />
 					<label for=mastercard>Mastercard</label>
 				</fieldset>
-				<label for=cardnumber>Kortnummer</label>
+				<label for=cardnumber>Kortnummer</label><br>
 				<input id=cardnumber name=cardnumber type=number placeholder="1234123412341234" required /><br>
-				
-				<label for=expirationdate>Giltlighetstid</label>
+				<br>
+				<label for=expirationdate>Giltlighetstid</label><br>
 				<input id=expirationmonth name=expirationmonth type=number placeholder="01" required />
 				<input id=expirationyear name=expirationyear type=number placeholder="20" required /> <br>
-				
-				<label for=cvc>CVC</label>
+				<br>
+				<label for=cvc>CVC</label><br>
 				<input id=cvc name=cvc type=nummer placeholder="000" required /><br>
-				
-				<label for=cardname>Namn på kortet</label>
+				<br>
+				<label for=cardname>Namn på kortet</label><br>
 				<input id=cardname name=cardname placeholder="Sam Morris" required /> <br>
 		</fieldset>
 		<fieldset>
 			<button type=submit>Betala</button>
-			
-			<!-- Generera kvittosida. -->
 		</fieldset>
 	</form>
 	</section>
