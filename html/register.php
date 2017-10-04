944becc18eea8fd88130420e60c0c8df7c2cc037
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-10-04 11:13:53 +0200

    maxlength on registration

--- html/register.php
+++ html/register.php
@@ -15,11 +15,11 @@ include 'navbar.php';
 			<form class="login" method="post" >
         <div class="login" >
   				<label><b>Username</b></label>
-          <input type="text" name="username" required><br>
+          <input type="text" name="username" maxlength=50 required><br>
   				<label><b>Password</b></label>
-          <input type="password" name="password" required><br>
+          <input type="password" name="password" maxlength=159 required><br>
   				<label><b>Address</b></label>
-          <input type="text" name="address" required><br>
+          <input type="text" name="address" maxlength=50 required><br>
 					<?php echo csrf_input_tag(); ?>
   				<button type="submit">Register</button>
 			<div style="background-color:#f1f1f1">
@@ -45,21 +45,25 @@ include 'navbar.php';
   			} elseif ($_POST['password'] == $_POST['username']) {
   				$error = "Your password can't be your username.";
   			} elseif (!(preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $_POST['password']))){
-				$error = "Password must contain letters and numbers.";
-			}else {
-  				$hashedpw = password_hash( $_POST['password'], PASSWORD_DEFAULT);
-  				$registered = $db->addUser($_POST['username'], $hashedpw, $_POST['address']);
-  				if($registered){
-            session_regenerate_id();
-            $_SESSION["loggedIn"]=true;
-            $_SESSION['username'] = $_POST['username'];
-  					echo "<script> window.location = 'index.php'; </script>";
-  				} else {
-  					$error = "Username already in use";
-  				}
-  			}
-        echo $error;
-  		}
+					$error = "Password must contain letters and numbers.";
+				} elseif (strlen($_POST['username']) > 50) {
+					$error = "Nice try, but we also check username length serverside.";
+				} elseif (strlen($_POST['username']) > 50) {
+					$error = "Nice try, but we also check address length serverside.";
+				} else {
+						$hashedpw = password_hash( $_POST['password'], PASSWORD_DEFAULT);
+						$registered = $db->addUser($_POST['username'], $hashedpw, $_POST['address']);
+						if($registered){
+							session_regenerate_id();
+							$_SESSION["loggedIn"]=true;
+							$_SESSION['username'] = $_POST['username'];
+							echo "<script> window.location = 'index.php'; </script>";
+						} else {
+							$error = "Username already in use";
+						}
+					}
+					echo $error;
+				}
     }
 		?>
 		</div>
